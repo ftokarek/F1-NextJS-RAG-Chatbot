2ad0751f711d3f7720fa883802a41d56bb8fcaec
Franciszek Tokarek <franciszektokarek@gmail.com>
2025-10-15 14:48:52 +0200

    frontend edits

--- f1-nextjs-rag-chatbot/app/page.tsx
+++ f1-nextjs-rag-chatbot/app/page.tsx
@@ -7,10 +7,37 @@ import {useChat} from "@ai-sdk/react"
 import type {UIMessage} from "@ai-sdk/react"
 
 const Home = () => {
+
+    const {append,isLoading,messages,input,handleInputChange,handleSubmit} = useChat()
+
+    const noMessages = false
+
+
+    
     return (
         <main>
-        <Image src={F1GPTLogo} width={250} alt="F1 RAG Chatbot Logo" />
+            <Image className="logo" src={F1GPTLogo} width={250} alt="F1 RAG Chatbot Logo" />
+            <section className={noMessages ? "" : "populated"}>
+                {noMessages ?(
+                    <>
+                    <p className="starter-text">The best place for Formula 1 fans!</p>
+                    <br/>
+                    {/*Prompt suggestions*/}
+                    </>
+                ) : (
+                    <>
+                       {/*Map messages onto text bubbles*/}
+                       {/*<LoadingBubble/>*/}
+                    </>
+                )}
+                
+            </section>
+            <form onSubmit={handleSubmit}>
+                    <input className="question-box" onChange={handleInputChange} value={input} placeholder="Ask me anything..."/>
+                    <input type="submit"/>
+            </form>
         </main>
+
     )
 }
 export default Home
