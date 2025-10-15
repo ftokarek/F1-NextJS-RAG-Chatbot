26c9eda3fbe4e185a21d13034efae193fe66f6ce
Franciszek Tokarek <franciszektokarek@gmail.com>
2025-10-15 13:28:39 +0200

    add: layout.tsx

---
+++ f1-nextjs-rag-chatbot/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css"
+
+export const metadata = {
+    title: "F1 RAG Chatbot",
+    description: "The place to go for all your F1 questions!"
+}
+
+const RootLayout = ({children}) => {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
